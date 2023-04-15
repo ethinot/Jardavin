c533ab76f13d8b41e1c4e93431e8aa968051b4a8
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2023-04-15 15:02:16 +0200

    Ajout fonctionnalités pour le temps

--- src/modele/temps/TempsModel.java
+++ src/modele/temps/TempsModel.java
@@ -3,14 +3,23 @@ package modele.temps;
 import modele.temps.composants.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Timer;
 
 public class TempsModel {
+    // Design pattern singleton pour que les temps soit le même partout dans le programme
+    private static TempsModel temps;
     private LocalDateTime tempsModel;
     private Timer miseAJourTemps;
     private int periodeActuel;
-    // Design pattern singleton pour que les temps soit le même partout dans le programme
-    private static TempsModel temps;
+    // Format pour formater les dates
+
+    private DateTimeFormatter formatJour = DateTimeFormatter.ofPattern("dd");
+    private DateTimeFormatter formatMois = formatMois = DateTimeFormatter.ofPattern("MMMM", Locale.FRANCE);
+    private DateTimeFormatter formatAnnee = formatAnnee = DateTimeFormatter.ofPattern("yyyy");
+    private DateTimeFormatter formatHeureMinuteSeconde = formatHeureMinuteSeconde = DateTimeFormatter.ofPattern("HH:mm:ss");
+
 
     public TempsModel (int _annee, int _mois, int _jour) {
         tempsModel = LocalDateTime.of(_annee, _mois, _jour, 6, 0, 0);
@@ -34,7 +43,17 @@ public class TempsModel {
         return temps;
     }
 
-    public LocalDateTime getTempsModel() { return tempsModel;}
+    public String getTempsHeureMinuteSeconde() { return tempsModel.format(formatHeureMinuteSeconde); }
+    public String getTempsJour() { return tempsModel.format(formatJour); }
+    public String getTempsMois() { return tempsModel.format(formatMois); }
+    public String getTempsAnne() { return tempsModel.format(formatAnnee); }
+
+    public int getTempsEnSeconde() {
+        String tempsEnEntier = String.valueOf(tempsModel.getYear()) + String.valueOf(tempsModel.getMonthValue()) + String.valueOf(tempsModel.getDayOfMonth()) +
+                String.valueOf(tempsModel.getHour()) + String.valueOf(tempsModel.getMinute()) + String.valueOf(tempsModel.getSecond());
+        int res = Integer.parseInt(tempsEnEntier);
+        return res;
+    }
 
     public void setPeriode(int periode, IncrementerTempsModel incrementeur) {
         periodeActuel = periode;
